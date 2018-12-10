7a29bafbd848ccccfc3e956f8f0a6fc719c3cca9
MikeLitoris <maxime.genaudet@gmail.com>
2018-12-10 22:18:32 +0100

    fix entrypoint

--- public/scripts/entrypoint.sh
+++ public/scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i -e "s/FLAPPY_SANTA_URL/$FLAPPY_SANTA_URL/g" /homepage/public/index.html
-sed -i -e "s/SANTARMINATOR_URL/$SANTARMINATOR_URL/g" /homepage/public/index.html
+sed -i -e "s|FLAPPY_SANTA_URL|$FLAPPY_SANTA_URL|g" /homepage/public/index.html
+sed -i -e "s|SANTARMINATOR_URL|$SANTARMINATOR_URL|g" /homepage/public/index.html
 cd homepage && npm install && node index.js > homepage.log
 
